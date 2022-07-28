fd34e6429005e788c37cfe72b2b853740a72104b
Devorah Ortega <devorahb.ortega@gmail.com>
2022-07-28 15:24:09 -0500

    Transfer commit

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,11 @@
+class DogsController < ApplicationController
+  def create
+    dog = Dog.new(
+      name: params["name"],
+      age: params["age"],
+      breed: params["breed"],
+    )
+    dog.save
+    render json: dog.as_json
+  end
+end
